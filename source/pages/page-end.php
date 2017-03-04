458b1b1869addb9e8ee5432937f78d2c95d2f893
Alexandr Ja <mr-spinn@outlook.com>
2017-03-04 17:31:08 +0600

    update script tags

--- source/pages/page-end.php
+++ source/pages/page-end.php
@@ -1,6 +1,7 @@
     <script src="//localhost:35729/livereload.js"></script>
     <!--third-components scripts-->
     <!--layout's scripts-->
-    <script src="components/components-scripts/collected_scripts/js/layout.js"></script>
+    <script src="components/components-libs/jquery-1.9.1/jquery-1.9.1.js"></script>
+    <script src="components/components-scripts/collected-scripts/layout.js"></script>
   </body>
 </html>
