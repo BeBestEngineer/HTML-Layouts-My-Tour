5dabffd8a5442f00ac20cc55c1625b1b647b3b83
Alexandr Ja <mr-spinn@outlook.com>
2017-03-05 20:27:37 +0600

    change title of project

--- source/pages/page-head.php
+++ source/pages/page-head.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
     <meta charset="UTF-8">
-    <title>TeaShop - eCommerce PSD Template</title>
+    <title>My tour</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <!--third-components scripts-->
     <link rel="stylesheet" href="components/components-libs/normalize-css-5.0.0/normalize-5.0.0.css">
