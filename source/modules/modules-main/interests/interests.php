e7b176ba6c22c240c53e5308f157ae0ed9fd1825
Alexandr Ja <mr-spinn@outlook.com>
2017-03-07 19:37:35 +0600

    markup for interests page without js dropdowns and carousels

--- source/modules/modules-main/interests/interests.php
+++ source/modules/modules-main/interests/interests.php
@@ -1,76 +1,179 @@
-<div class="  flex__container  flex__item--stretch">
-  <main class="main-modules  main-modules--interests  flex__container  grid__container--medium">
+<div class="flex__container  flex__item--stretch  main-wrapper">
+  <main class="main-modules  interests  grid__container--medium">
+    <div class="grid__col">
+      <b class="title">Настройка интересов</b>
 
 
-      <div class="flex__item--equal-width  grid__col">
-        Grid-cell
+      <div class="tab">
+        <a class="links  tablinks"  href="javascript:void(0)"  onclick="openCity(event, 'selection-1')">Подборка</a>
+        <a class="links  tablinks"  href="javascript:void(0)"  onclick="openCity(event, 'selection-2')">Подборка 2</a>
       </div>
-      <div class="flex__item--equal-width  grid__col">
-        Grid-cell
-      </div>
-      <div class="flex__item--equal-width  grid__col">
-        <a class="links" href="#">
-          <svg class="icons__svg--text  allign-middle" width="20" height="20" viewBox="0 0 1792 1792">
-            <path d="M1853 520q23 64-150 294-24 32-65 85-78 100-90 131-17 41 14 81 17 21 81 82h1l1 1 1 1 2 2q141 131 191 221 3 5 6.5 12.5t7 26.5-.5 34-25 27.5-59 12.5l-256 4q-24 5-56-5t-52-22l-20-12q-30-21-70-64t-68.5-77.5-61-58-56.5-15.5q-3 1-8 3.5t-17 14.5-21.5 29.5-17 52-6.5 77.5q0 15-3.5 27.5t-7.5 18.5l-4 5q-18 19-53 22h-115q-71 4-146-16.5t-131.5-53-103-66-70.5-57.5l-25-24q-10-10-27.5-30t-71.5-91-106-151-122.5-211-130.5-272q-6-16-6-27t3-16l4-6q15-19 57-19l274-2q12 2 23 6.5t16 8.5l5 3q16 11 24 32 20 50 46 103.5t41 81.5l16 29q29 60 56 104t48.5 68.5 41.5 38.5 34 14 27-5q2-1 5-5t12-22 13.5-47 9.5-81 0-125q-2-40-9-73t-14-46l-6-12q-25-34-85-43-13-2 5-24 17-19 38-30 53-26 239-24 82 1 135 13 20 5 33.5 13.5t20.5 24 10.5 32 3.5 45.5-1 55-2.5 70.5-1.5 82.5q0 11-1 42t-.5 48 3.5 40.5 11.5 39 22.5 24.5q8 2 17 4t26-11 38-34.5 52-67 68-107.5q60-104 107-225 4-10 10-17.5t11-10.5l4-3 5-2.5 13-3 20-.5 288-2q39-5 64 2.5t31 16.5z"/>
-          </svg>
-          <span class="allign-middle">Вконтакте</span>
-        </a>
-        <a class="links" href="#">
-          <svg class="icons__svg--text  allign-middle" width="20" height="20" viewBox="0 0 1792 1792">
-            <path d="M1579 128q35 0 60 25t25 60v1366q0 35-25 60t-60 25h-391v-595h199l30-232h-229v-148q0-56 23.5-84t91.5-28l122-1v-207q-63-9-178-9-136 0-217.5 80t-81.5 226v171h-200v232h200v595h-735q-35 0-60-25t-25-60v-1366q0-35 25-60t60-25h1366z"/>
-          </svg>
-          <span class="allign-middle">Facebook</span>
-        </a>
-        <a class="links" href="#">
-          <svg class="icons__svg--text  allign-middle" width="20" height="20" viewBox="0 0 1792 1792">
-            <path d="M1490 1426v-648h-135q20 63 20 131 0 126-64 232.5t-174 168.5-240 62q-197 0-337-135.5t-140-327.5q0-68 20-131h-141v648q0 26 17.5 43.5t43.5 17.5h1069q25 0 43-17.5t18-43.5zm-284-533q0-124-90.5-211.5t-218.5-87.5q-127 0-217.5 87.5t-90.5 211.5 90.5 211.5 217.5 87.5q128 0 218.5-87.5t90.5-211.5zm284-360v-165q0-28-20-48.5t-49-20.5h-174q-29 0-49 20.5t-20 48.5v165q0 29 20 49t49 20h174q29 0 49-20t20-49zm174-208v1142q0 81-58 139t-139 58h-1142q-81 0-139-58t-58-139v-1142q0-81 58-139t139-58h1142q81 0 139 58t58 139z"/>
-          </svg>
-          <span class="allign-middle">Instagram</span>
-        </a>
-        <a class="links" href="#">
-          <svg enable-background="new 0 0 30 30" height="18px" viewBox="0 0 30 30" width="18px" >
-            <path d="M22,15c-1,0-3,2-7,2s-6-2-7-2c-1.104,0-2,0.896-2,2c0,1,0.568,1.481,1,1.734C8.185,19.427,12,21,12,21l-4.25,5.438  c0,0-0.75,0.935-0.75,1.562c0,1.104,0.896,2,2,2c1.021,0,1.484-0.656,1.484-0.656S14.993,23.993,15,24  c0.007-0.007,4.516,5.344,4.516,5.344S19.979,30,21,30c1.104,0,2-0.896,2-2c0-0.627-0.75-1.562-0.75-1.562L18,21  c0,0,3.815-1.573,5-2.266C23.432,18.481,24,18,24,17C24,15.896,23.104,15,22,15z" id="K"/>
-            <path d="M15,0c-3.866,0-7,3.134-7,7s3.134,7,7,7c3.865,0,7-3.134,7-7S18.865,0,15,0z M15,10.5c-1.933,0-3.5-1.566-3.5-3.5  c0-1.933,1.567-3.5,3.5-3.5c1.932,0,3.5,1.567,3.5,3.5C18.5,8.934,16.932,10.5,15,10.5z" id="O"/>
-          </svg>
-          <span class="allign-middle">Однокласники</span>
-        </a>
-        <a class="links" href="#">
-          <svg class="icons__svg--text  allign-middle" width="20" height="20" viewBox="0 0 1792 1792">
-            <path d="M1853 520q23 64-150 294-24 32-65 85-78 100-90 131-17 41 14 81 17 21 81 82h1l1 1 1 1 2 2q141 131 191 221 3 5 6.5 12.5t7 26.5-.5 34-25 27.5-59 12.5l-256 4q-24 5-56-5t-52-22l-20-12q-30-21-70-64t-68.5-77.5-61-58-56.5-15.5q-3 1-8 3.5t-17 14.5-21.5 29.5-17 52-6.5 77.5q0 15-3.5 27.5t-7.5 18.5l-4 5q-18 19-53 22h-115q-71 4-146-16.5t-131.5-53-103-66-70.5-57.5l-25-24q-10-10-27.5-30t-71.5-91-106-151-122.5-211-130.5-272q-6-16-6-27t3-16l4-6q15-19 57-19l274-2q12 2 23 6.5t16 8.5l5 3q16 11 24 32 20 50 46 103.5t41 81.5l16 29q29 60 56 104t48.5 68.5 41.5 38.5 34 14 27-5q2-1 5-5t12-22 13.5-47 9.5-81 0-125q-2-40-9-73t-14-46l-6-12q-25-34-85-43-13-2 5-24 17-19 38-30 53-26 239-24 82 1 135 13 20 5 33.5 13.5t20.5 24 10.5 32 3.5 45.5-1 55-2.5 70.5-1.5 82.5q0 11-1 42t-.5 48 3.5 40.5 11.5 39 22.5 24.5q8 2 17 4t26-11 38-34.5 52-67 68-107.5q60-104 107-225 4-10 10-17.5t11-10.5l4-3 5-2.5 13-3 20-.5 288-2q39-5 64 2.5t31 16.5z"/>
-          </svg>
-          <span class="allign-middle">Вконтакте</span>
-        </a>
-        <a class="links" href="#">
-          <svg class="icons__svg--text  allign-middle" width="20" height="20" viewBox="0 0 1792 1792">
-            <path d="M1579 128q35 0 60 25t25 60v1366q0 35-25 60t-60 25h-391v-595h199l30-232h-229v-148q0-56 23.5-84t91.5-28l122-1v-207q-63-9-178-9-136 0-217.5 80t-81.5 226v171h-200v232h200v595h-735q-35 0-60-25t-25-60v-1366q0-35 25-60t60-25h1366z"/>
-          </svg>
-          <span class="allign-middle">Facebook</span>
-        </a>
-        <a class="links" href="#">
-          <svg class="icons__svg--text  allign-middle" width="20" height="20" viewBox="0 0 1792 1792">
-            <path d="M1490 1426v-648h-135q20 63 20 131 0 126-64 232.5t-174 168.5-240 62q-197 0-337-135.5t-140-327.5q0-68 20-131h-141v648q0 26 17.5 43.5t43.5 17.5h1069q25 0 43-17.5t18-43.5zm-284-533q0-124-90.5-211.5t-218.5-87.5q-127 0-217.5 87.5t-90.5 211.5 90.5 211.5 217.5 87.5q128 0 218.5-87.5t90.5-211.5zm284-360v-165q0-28-20-48.5t-49-20.5h-174q-29 0-49 20.5t-20 48.5v165q0 29 20 49t49 20h174q29 0 49-20t20-49zm174-208v1142q0 81-58 139t-139 58h-1142q-81 0-139-58t-58-139v-1142q0-81 58-139t139-58h1142q81 0 139 58t58 139z"/>
-          </svg>
-          <span class="allign-middle">Instagram</span>
-        </a>
-        <a class="links" href="#">
-          <svg enable-background="new 0 0 30 30" height="18px" viewBox="0 0 30 30" width="18px" >
-            <path d="M22,15c-1,0-3,2-7,2s-6-2-7-2c-1.104,0-2,0.896-2,2c0,1,0.568,1.481,1,1.734C8.185,19.427,12,21,12,21l-4.25,5.438  c0,0-0.75,0.935-0.75,1.562c0,1.104,0.896,2,2,2c1.021,0,1.484-0.656,1.484-0.656S14.993,23.993,15,24  c0.007-0.007,4.516,5.344,4.516,5.344S19.979,30,21,30c1.104,0,2-0.896,2-2c0-0.627-0.75-1.562-0.75-1.562L18,21  c0,0,3.815-1.573,5-2.266C23.432,18.481,24,18,24,17C24,15.896,23.104,15,22,15z" id="K"/>
-            <path d="M15,0c-3.866,0-7,3.134-7,7s3.134,7,7,7c3.865,0,7-3.134,7-7S18.865,0,15,0z M15,10.5c-1.933,0-3.5-1.566-3.5-3.5  c0-1.933,1.567-3.5,3.5-3.5c1.932,0,3.5,1.567,3.5,3.5C18.5,8.934,16.932,10.5,15,10.5z" id="O"/>
-          </svg>
-          <span class="allign-middle">Однокласники</span>
-        </a>
+      <div   class="tabs-wrapper"                    id="tabs-wrapper-id">
+        <div class="tabcontent  tabcontent--active"  id="selection-1">
+          <div class="interests__form-wrapper">
+            <form action="" class="interests__form">
+
+
+              <div class="form__container">
+                <div class="flex__container">
+
+                  <div class="flex__container  flex__direction--column  flex__item--custom-width-1-0  form__selection-name-container">
+                    <label class="form__label" for="selection-name">Название подборки</label>
+                    <input class="form__input" type="text" id="selection-name" placeholder="Введите название подборки">
+                  </div>
+
+
+                  <div class="flex__container  flex__direction--column  flex__item--custom-width-1-0">
+                    <label class="form__label" for="selection-name">Ваш город</label>
+                    <input class="form__input" type="text" id="selection-name" placeholder="Введите название вашего города">
+                  </div>
+
+
+                  <div class="flex__container  flex__direction--row-reverse  flex__align-items--flex-end  flex__item--custom-width-2-0">
+                    <button class="form__button  form__button--submit" type="submit">
+                      <span class="allign-middle">Смотреть результаты</span>
+                      <span class="allign-middle  icons__arrow--180-deg">
+                        <svg class="" viewBox="0 0 26 26" enable-background="new 0 0 26 26" width="12" height="12">
+                          <path d="M16.7,22.7l9-9c0.2-0.2,0.3-0.5,0.3-0.7c0-0.3-0.1-0.5-0.3-0.7l-9-9C16.5,3.1,16.3,3,16,3s-0.5,0.1-0.7,0.3l-1.4,1.4  c-0.4,0.4-0.4,1,0,1.4l4,4c0.3,0.3,0.1,0.9-0.4,0.9H1c-0.6,0-1,0.4-1,1v2c0,0.6,0.4,1,1,1h16.6c0.4,0,0.7,0.5,0.4,0.9l-4,4  c-0.4,0.4-0.4,1,0,1.4l1.4,1.4c0.2,0.2,0.4,0.3,0.7,0.3C16.3,23,16.5,22.9,16.7,22.7z"/>
+                        </svg>
+                      </span>
+                    </button>
+                  </div>
+
+                </div>
+              </div>
+
+
+              <div class="form__container">
+                  <label class="form__label">Вы можете задать несколько стран или регионов для подборки</label>
+                  <input class="form__input  form__input--regions" type="text" placeholder="Выберите регион мира и страну для добавления к подборке" value="Франция Германия">
+
+                <div class="flex__container  flex__direction--column  form__area">
+                  <span class="form__region-name">Европа</span>
+                  <div class="flex__container">
+                    <div    class="pills">
+                      <div class="image-wrapper  pills__image--flag  allign-middle">
+                        <img class="image" src="/myTour.loc/source/images/flags/flag-france.png" alt="flag-france" >
+                      </div>
+                      <span  class="pills__text          allign-middle">Франция</span>
+                      <a     class="pills__icon-wrapper  allign-middle" href="#"></a>
+                    </div>
+                    <div    class="pills">
+                      <div class="image-wrapper  pills__image--flag  allign-middle">
+                        <img class="image" src="/myTour.loc/source/images/flags/flag-germany.png" alt="flag-germany" >
+                      </div>
+                      <span  class="pills__text          allign-middle">Германия</span>
+                      <a     class="pills__icon-wrapper  allign-middle" href="#"></a>
+                    </div>
+                  </div>
+                </div>
+                <div class="flex__container">
+                  <button class="form__button  form__button--select-region" type="button">Добавить регион</button>
+                  <button class="form__button  form__button--select-region" type="button">Добавить страну</button>
+                </div>
+              </div>
+              
+
+
+              <div class="form__container">
+                <div class="flex__container  flex__direction--column">
+                  <div class="flex__container  form__container">
+
+                    <div class="flex__container  flex__direction--column  flex__item--custom-width-270-0">
+                      <div class="flex__container">
+<!--                      960px / 135px = 7.111-->
+                        <label class="form__label"  for="departure-date">Дата вылета</label>
+                        <label class="form__label  hide__label"  for="arrival-date">Дата прилёта</label>
+                      </div>
+
+                      <div class="flex__container">
+<!--                      135px -->
+                        <input class="form__input  form__input--departure-date"  type="text"  id="departure-date" placeholder="дата вылета">
+                        <input class="form__input  form__input--arrival-date"  type="text"  id="arrival-date" placeholder="дата прилёта">
+                      </div>
+                    </div>
+
+
+                    <div class="flex__container  flex__direction--column  flex__item--custom-width-170-0">
+<!--                      todo: вместо flex__direction--column указать flex__direction т.е. без --column-->
+<!--                      960px / 170px = 5.647-->
+                      <label class="form__label"  for="dates">На сколько</label>
+                      <input class="form__input"  type="text"  id="dates" placeholder="7-14 ночей">
+                    </div>
+
+
+                    <div class="flex__container  flex__direction--column  flex__item--custom-width-214-0">
+<!--                      960px / 107px = 8.972-->
+                      <div class="flex__container  ">
+                        <label class="form__label" for="passengers">Количество пассажиров</label>
+                        <label class="form__label  hide__label" for="passengers-childs">Количество пассажиров</label>
+                      </div>
+                      <div class="flex__container  ">
+                        <input class="form__input  form__input--passengers-parents" type="text" id="passengers-parents" placeholder="взрослые" value="1 взрослый">
+                        <input class="form__input  form__input--passengers-childs" type="text" id="passengers-childs" placeholder="дети" value="0 детей">
+                      </div>
+                    </div>
+
+
+                    <div class="flex__container  flex__direction--column  flex__item--custom-width-265-0">
+<!--                      960px / 265px = 3.623-->
+                      <label class="form__label"  for="price">Стоимость тура</label>
+                      <input class="form__input"  type="text"  id="price" placeholder="Укажите стоимость тура" value="50 000 Р - 150 000 Р">
+                    </div>
+                  </div>
+
+
+                  <div class="flex__container">
+                    <div class="flex__container  flex__direction--column  flex__item--custom-width-160-0">
+                      <label class="form__label"  for="hotel-stars">Категория отелей от</label>
+                      <input class="form__input"  type="text"  id="hotel-stars" placeholder="" value="5 stars">
+                      <span class="icons__chevron icons__chevron--bottom"></span>
+                    </div>
+                    <div class="flex__container  flex__direction--column  flex__item--custom-width-185-0">
+                      <label class="form__label"  for="hotel-rating">Рейтинг TopHotels</label>
+                      <input class="form__input"  type="text"  id="hotel-rating" placeholder="" value="не ниже 4.0">
+                    </div>
+                    <div class="flex__container  flex__direction--column  flex__item--custom-width-125-0">
+                      <label class="form__label"  for="nutrition">Тип питания</label>
+                      <input class="form__input"  type="text"  id="nutrition" placeholder="Выберите питание">
+                    </div>
+                    <div class="flex__container  flex__direction--column  flex__item--custom-width-225-0">
+                      <label class="form__label"  for="location">Расположение</label>
+                      <input class="form__input"  type="text"  id="location" placeholder="Выберите расположение">
+                    </div>
+                    <!--
+                      todo: сделать input как в яндекс авиа
+                      todo: скрыть инпут и добавить readonly="readonly", значения вставлять в div
+                     -->
+                    <div class="flex__container  flex__direction--row-reverse  flex__align-items--flex-end  flex__item--custom-width-225-0">
+                      <button class="form__button  form__button--clear-selection" type="button">
+                        <span class="icons__cross-in-circle  allign-middle"></span>
+                        <span class="allign-middle">Удалить подборку</span>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div class="tabcontent"                      id="selection-2">
+          Подборка 2
+        </div>
       </div>
 
 
+    </div>
   </main>
 
+<!--todo: hovers? colors? backgrounds? js?-->
+  <aside class="flex__item--equal-width  grid__col    flex__item--order--1">
+    <!--    Empty Aside-->
+  </aside>
 
-  <nav class="flex__item--order--1  flex__item--stretch  grid__col">
-    HolyGrail-nav
-  </nav>
 
-
-  <aside class="flex__item--stretch  grid__col">
-    HolyGrail-ads
+  <aside class="flex__item--equal-width  grid__col">
+    <!--    Empty Aside-->
   </aside>
 </div>
+
