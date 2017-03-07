502d5cef98679472f93028fe6a6677aec7bcb57b
Alexandr Ja <mr-spinn@outlook.com>
2017-03-07 19:37:54 +0600

    add header markup

--- source/modules/modules-header/header-1/header-1.php
+++ source/modules/modules-header/header-1/header-1.php
@@ -1,3 +1,36 @@
-<header class="header-1  grid__col">
-  <h2>Header</h2>  
+<header class="header-1  flex__container">
+  <div class="flex__container  grid__container--medium">
+    <nav class="flex__item--stretch  grid__col  main-nav">
+      <ul>
+        <li class="main-nav__item">
+          <a class="links  main-nav__link" href="#">Моя лента</a>
+        </li>
+        <li class="main-nav__item">
+          <a class="links  main-nav__link" href="#">Интересы</a>
+        </li>
+        <li class="main-nav__item">
+          <a class="links  main-nav__link" href="#">Направления</a>
+        </li>
+        <li class="main-nav__item">
+          <a class="links  main-nav__link" href="#">Подбор тура</a>
+        </li>
+        <li class="main-nav__item">
+          <a class="links  main-nav__link" href="#">Заявка</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+
+
+  <aside class="flex__item--equal-width  grid__col  flex__item--order--1  flex__container  flex__justify-content--flex-end">
+<!--    Empty Aside-->
+    <a href="#" class="logo-link  image-wrapper">
+      <img class="image" src="images/logo/logo.jpg" alt="logo">
+    </a>
+  </aside>
+
+
+  <aside class="flex__item--equal-width  grid__col">
+<!--    Empty Aside-->
+  </aside>
 </header>
