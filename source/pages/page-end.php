8e9baf10fa3edb68b6ca8dade3614aba13ecf4c6
Alexandr Ja <mr-spinn@outlook.com>
2017-03-10 17:20:14 +0600

    add script tags for moment.js, pikaday.js, noUIslide.js, wNumb.js

--- source/pages/page-end.php
+++ source/pages/page-end.php
@@ -1,5 +1,9 @@
     <script src="//localhost:35729/livereload.js"></script>
     <!--third-components scripts-->
+    <script src="components/components-libs/moment/moment.js"></script>
+    <script src="components/components-libs/pikaday/pikaday.js"></script>
+    <script src="components/components-libs/noUIslider/nouislider.js"></script>
+    <script src="components/components-libs/wnumb-1.1.0/wNumb.js"></script>
     <!--layout's scripts-->
     <script src="components/components-scripts/collected-scripts/layout.js"></script>
   </body>
