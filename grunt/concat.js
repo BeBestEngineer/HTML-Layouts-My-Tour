264c8bbb98be18760f1e0db10d85ae6841952638
Alexandr Ja <mr-spinn@outlook.com>
2017-03-10 16:45:50 +0600

    change concat task for moment.js  and pikadate.js

--- grunt/concat.js
+++ grunt/concat.js
@@ -17,9 +17,22 @@ module.exports = {
     ],
         dest: 'source/components/components-styles/collected-styles/css/vendor.css'
   },
+  sourcePikADayStyles: {
+    src: [
+      'source/components/components-styles/variables.scss',
+      'source/components/components-libs/pikaday/pikaday.scss'
+    ],
+    dest: 'source/components/components-libs/pikaday/pikaday-with-variables.scss'
+  },
   sourceLayoutScripts: {
-    src: ['source/components/components-scripts/layout_inception.js', 'source/modules/**/*.js', 'source/components/components-scripts/layout_completion.js'],
-        dest: 'source/components/components-scripts/collected-scripts/layout.js'
+    src: [
+      'source/components/components-scripts/layout_inception.js',
+      'source/components/components-scripts/settings_moment.js',
+      'source/components/components-scripts/*.js',
+      'source/modules/**/*.js',
+      'source/components/components-scripts/layout_completion.js'
+    ],
+    dest: 'source/components/components-scripts/collected-scripts/layout.js'
   },
   sourceVendorScripts: {
     src: [
