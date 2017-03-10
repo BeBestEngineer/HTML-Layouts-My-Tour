59ce6a08893c0a4219b203721fb79b9a45e2f236
Alexandr Ja <mr-spinn@outlook.com>
2017-03-10 20:01:08 +0600

    delete console.log from script for rating

--- source/components/components-scripts/elements_rating.js
+++ source/components/components-scripts/elements_rating.js
@@ -11,11 +11,8 @@
 		// console.log ( singleRatingElement );
 
 		if ( singleRatingElement.classList.contains('rating--5-stars') ) {
-			// console.log ( singleRatingElement.firstElementChild );
 
 			iconOriginal = singleRatingElement.firstElementChild;
-
-			// console.log(iconOriginal);
 
 			iconCloneColor = iconOriginal.cloneNode(true);
 			iconCloneColor.classList.add('icons__svg--color');
@@ -23,14 +20,13 @@
 			iconCloneNoColor = iconOriginal.cloneNode(true);
 			iconCloneNoColor.classList.add('icons__svg--nocolor');
 
-			// console.log(iconCloneColor);
-			// console.log(iconCloneNoColor);
-
-			// singleRatingElement.innerHTML = '';
+			singleRatingElement.innerHTML = '';
 
 			var j;
 			for ( j = 1; j <= 4; j++) {
-				console.log( singleRatingElement.appendChild( iconCloneColor ) );
+				singleRatingElement.appendChild( iconOriginal.cloneNode(true) );
+				// 1. Отклонировать нужное число раз и потом вставить
+				// 2. вставлять оп одному
 			}
 		}
 	}
