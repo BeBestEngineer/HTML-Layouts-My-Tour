1b4fde5765059d191446b8b2f61eb6b7f9ed1318
Alexandr Ja <mr-spinn@outlook.com>
2017-03-10 12:46:48 +0600

    dropdown menu test

--- source/components/components-scripts/elements_dropdowns-menu.js
+++ source/components/components-scripts/elements_dropdowns-menu.js
@@ -1,11 +1,21 @@
 /* When the user clicks on the button,
  toggle between hiding and showing the dropdown content */
 function openDropdownMenu( event, menuId, menuClass ) {
-	document.getElementById( menuId ).classList.toggle("show");
-
 	openDropdownMenu.EVENT = event;
 	openDropdownMenu.MENU_ID = menuId;
 	openDropdownMenu.MENU_CLASS = menuClass;
+
+	//Закрыть все открытые dropdown меню
+  console.log('start closeDropdownMenu... from openDropdownMenu');
+
+	// если сделан клик по кнопке под которой открыто выпадающее меню( присвоен класс show ) то не выполнять closeDropdownMenu
+	//иначе если сделан клик по кнопке под которой не открыто меню то выполнить closeDropdownMenu
+	if ( !document.getElementById( openDropdownMenu.MENU_ID ).classList.contains('show')) {
+		closeDropdownMenu();
+	}
+
+  document.getElementById( menuId ).classList.toggle("show");
+
 }
 
 
@@ -13,18 +23,34 @@ function openDropdownMenu( event, menuId, menuClass ) {
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function( event ) {
-	if ( !event.target.matches( '#' + openDropdownMenu.EVENT.target.id ) && ( event.target.offsetParent.id !== openDropdownMenu.MENU_ID ) ) {
-
-		var dropdowns = document.getElementsByClassName( openDropdownMenu.MENU_CLASS );
-		var i;
-		for (i = 0; i < dropdowns.length; i++) {
-			var openDropdown = dropdowns[i];
-			if (openDropdown.classList.contains('show')) {
-				openDropdown.classList.remove('show');
-			}
-		}
+  //todo: переделать через Методы contains или compareDocumentPosition
+	if ( !openDropdownMenu.EVENT && !openDropdownMenu.MENU_ID ) {
+		return false;
 	}
+  if ( !event.target.matches( '#' + openDropdownMenu.EVENT.target.id ) && ( event.target.offsetParent.id !== openDropdownMenu.MENU_ID ) ) {
+
+		console.log('start closeDropdownMenu... from window.onclick');
+    closeDropdownMenu();
+
+  }
 }
 
 
 
+function closeDropdownMenu () {
+	if ( !openDropdownMenu.MENU_CLASS ) {
+		return false;
+	}
+
+  var dropdowns = document.getElementsByClassName( openDropdownMenu.MENU_CLASS );
+
+  console.log( openDropdownMenu.MENU_ID );
+
+  var i;
+  for (i = 0; i < dropdowns.length; i++) {
+    var openDropdown = dropdowns[i];
+    if (openDropdown.classList.contains('show')) {
+      openDropdown.classList.remove('show');
+    }
+  }
+}
