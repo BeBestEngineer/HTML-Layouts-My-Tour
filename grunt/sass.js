7f2b53abba411d1f70a35f47d04adc7f605d0304
Alexandr Ja <mr-spinn@outlook.com>
2017-03-10 16:46:49 +0600

    change sass task for pikadate.js

--- grunt/sass.js
+++ grunt/sass.js
@@ -1,7 +1,9 @@
 module.exports = {
   build: {
+    //todo сделать отдельную задачу для watch и build
     files: {
-      'source/components/components-styles/collected-styles/css/layout.css': 'source/components/components-styles/collected-styles/scss/layout.scss'
+      'source/components/components-styles/collected-styles/css/layout.css': 'source/components/components-styles/collected-styles/scss/layout.scss',
+      'source/components/components-libs/pikaday/pikaday.css': 'source/components/components-libs/pikaday/pikaday-with-variables.scss'
     }
   }
-};
+}; 
