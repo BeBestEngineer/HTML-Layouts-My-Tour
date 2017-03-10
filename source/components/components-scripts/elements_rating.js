3f5278c547ae25587c09823e8915e58e50feb44b
Alexandr Ja <mr-spinn@outlook.com>
2017-03-10 20:46:25 +0600

    use createDocumentFragment for rating script

--- source/components/components-scripts/elements_rating.js
+++ source/components/components-scripts/elements_rating.js
@@ -2,32 +2,37 @@
 (function () {
 	var ratings = document.getElementsByClassName('rating');
 	var iconOriginal;
-	var iconCloneColor, iconCloneNoColor, icons = document.createElement('div');
+	var iconCloneColor, iconCloneNoColor;
+	var iconsColor 	 = document.createDocumentFragment(),
+			iconsNoColor = document.createDocumentFragment();
 
 	var i;
 	for (i = 0; i < ratings.length; i++) {
 		var singleRatingElement = ratings[i];
 
-		// console.log ( singleRatingElement );
-
 		if ( singleRatingElement.classList.contains('rating--5-stars') ) {
-			var j;
-			for ( j = 1; j <= 4; j++) {
+			iconOriginal = singleRatingElement.firstElementChild;
 
-				iconOriginal = singleRatingElement.firstElementChild;
+			var iconCloneColorNumbers;
+			for ( iconCloneColorNumbers = 1; iconCloneColorNumbers <= 4; iconCloneColorNumbers++) {
 
 				iconCloneColor = iconOriginal.cloneNode(true);
 				iconCloneColor.classList.add('icons__svg--color');
-
-				// iconCloneNoColor = iconOriginal.cloneNode(true);
-				// iconCloneNoColor.classList.add('icons__svg--nocolor');
-
-				// 1. Отклонировать нужное число раз и потом вставить
-				// 2. вставлять оп одному
-				icons.appendChild( iconCloneColor );
+				iconsColor.appendChild( iconCloneColor );
 			}
+
+			var iconCloneNoColorNumbers;
+			for ( iconCloneNoColorNumbers = 1; iconCloneNoColorNumbers <= 1; iconCloneNoColorNumbers++) {
+
+				iconCloneNoColor = iconOriginal.cloneNode(true);
+				iconCloneNoColor.classList.add('icons__svg--nocolor');
+				iconsNoColor.appendChild( iconCloneNoColor );
+			}
+
+
 			singleRatingElement.innerHTML = '';
-			singleRatingElement.appendChild( icons );
+			singleRatingElement.appendChild( iconsColor );
+			singleRatingElement.appendChild( iconsNoColor );
 		}
 	}
 })();
