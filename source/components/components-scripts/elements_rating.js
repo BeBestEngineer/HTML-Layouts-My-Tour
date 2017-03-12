160120743f18b767749e188937fc07fefdc9648c
Alexandr Ja <mr-spinn@outlook.com>
2017-03-12 14:39:25 +0600

    refactor elements_rating.js

--- source/components/components-scripts/elements_rating.js
+++ source/components/components-scripts/elements_rating.js
@@ -1,7 +1,6 @@
 //todo сделать модули для всех компонентов
 (function () {
 	var ratings = document.getElementsByClassName('rating');
-	var iconOriginal;
 	var iconCloneColor, iconCloneNoColor;
 	var iconsColor 	 = document.createDocumentFragment(),
 			iconsNoColor = document.createDocumentFragment();
@@ -10,29 +9,44 @@
 	for (i = 0; i < ratings.length; i++) {
 		var singleRatingElement = ratings[i];
 
+
 		if ( singleRatingElement.classList.contains('rating--5-stars') ) {
-			iconOriginal = singleRatingElement.firstElementChild;
-
-			var iconCloneColorNumbers;
-			for ( iconCloneColorNumbers = 1; iconCloneColorNumbers <= 4; iconCloneColorNumbers++) {
-
-				iconCloneColor = iconOriginal.cloneNode(true);
-				iconCloneColor.classList.add('icons__svg--color');
-				iconsColor.appendChild( iconCloneColor );
-			}
-
-			var iconCloneNoColorNumbers;
-			for ( iconCloneNoColorNumbers = 1; iconCloneNoColorNumbers <= 1; iconCloneNoColorNumbers++) {
-
-				iconCloneNoColor = iconOriginal.cloneNode(true);
-				iconCloneNoColor.classList.add('icons__svg--nocolor');
-				iconsNoColor.appendChild( iconCloneNoColor );
-			}
-
-
-			singleRatingElement.innerHTML = '';
-			singleRatingElement.appendChild( iconsColor );
-			singleRatingElement.appendChild( iconsNoColor );
+			insertElements( 5, 5 );
+		} else if ( singleRatingElement.classList.contains('rating--4-stars') ) {
+			insertElements( 4, 5 );
+		} else if ( singleRatingElement.classList.contains('rating--3-stars') ) {
+			insertElements( 3, 5 );
+		} else if ( singleRatingElement.classList.contains('rating--2-stars') ) {
+			insertElements( 2, 5 );
 		}
+	}
+
+
+
+
+	function insertElements( stars, allStars ) {
+		var iconOriginal = singleRatingElement.firstElementChild;
+		var starsColor = stars*1;
+		var starsNoColor = allStars*1 - starsColor*1;
+
+		var iconCloneColorNumbers;
+		for ( iconCloneColorNumbers = 1; iconCloneColorNumbers <= starsColor; iconCloneColorNumbers++) {
+
+			iconCloneColor = iconOriginal.cloneNode(true);
+			iconCloneColor.classList.add('icons__svg--color');
+			iconsColor.appendChild( iconCloneColor );
+		}
+
+		var iconCloneNoColorNumbers;
+		for ( iconCloneNoColorNumbers = 1; iconCloneNoColorNumbers <= starsNoColor; iconCloneNoColorNumbers++) {
+
+			iconCloneNoColor = iconOriginal.cloneNode(true);
+			iconCloneNoColor.classList.add('icons__svg--nocolor');
+			iconsNoColor.appendChild( iconCloneNoColor );
+		}
+
+		singleRatingElement.innerHTML = '';
+		singleRatingElement.appendChild( iconsColor );
+		singleRatingElement.appendChild( iconsNoColor );
 	}
 })();
