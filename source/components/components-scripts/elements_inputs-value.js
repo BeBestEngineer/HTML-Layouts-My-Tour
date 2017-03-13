6923db5013920e1691b1aa7382cac106ccc301a5
Alexandr Ja <mr-spinn@outlook.com>
2017-03-13 10:34:14 +0600

    testing add passengers value to form

--- source/components/components-scripts/elements_inputs-value.js
+++ source/components/components-scripts/elements_inputs-value.js
@@ -1,34 +1,35 @@
-function ChangeInputValueMore( e, inputId ) {
+function ChangeInputValueMore( e, inputId, outputId, textId ) {
   var buttonMore = document.getElementById( e.target.id );
   var input      = document.getElementById( inputId );
 
   if ( input && buttonMore ) {
-    var inputValue;
+    var inputValue = input.getAttribute("value");
 
-    inputValue = input.getAttribute("value");		
     inputValue = inputValue*1 + 1;
     input.setAttribute( "value", inputValue );
+
+    setValuePassengers( input, inputValue, outputId, textId );
   };
 };
 
 
 
 
-function ChangeInputValueLess( e, inputId ) {
+function ChangeInputValueLess( e, inputId, outputId, textId ) {
   var buttonLess = document.getElementById( e.target.id );
   var input      = document.getElementById( inputId );
 
   if ( input && buttonLess ) {
-    var inputValue;
+    var inputValue = input.getAttribute("value");
 
-    inputValue = input.getAttribute("value");
-    
-    if ( inputValue >= 2 ) {
+    if ( inputValue > 1 ) {
+      // console.log( inputValue );
       inputValue = inputValue*1 - 1;
       input.setAttribute( "value", inputValue );
     }
+      inputValue = inputValue*1;
 
-    // setValueOuterInput( inputValue );
+    setValuePassengers( input, inputValue, outputId, textId );
   };
 };
 
@@ -36,6 +37,45 @@ function ChangeInputValueLess( e, inputId ) {
 
 
 
-function setValueOuterInput( inputValue ) {
-  var outerInput = document.getElementById( e.target.id );
-};
+function setValuePassengers( input, inputValue, outputId, textId ) {
+  //Поле в которое будет вставляться числовое значение
+  var output = document.getElementById( outputId );
+  //Поле в которое будет вставляться строка содержащая текст для родителей или детей
+  var text   = document.getElementById( textId );
+
+  output.setAttribute( "value", inputValue );
+
+  text.innerHTML = '';
+
+  switch ( input.id ) {
+    case 'widget__passengers-range-parents--input':
+      text.innerHTML = setTextPassengersParents( inputValue );
+      break;
+    case 'widget__passengers-range-childs--input':
+      text.innerHTML = setTextPassengersChilds( inputValue );
+      break;
+  }
+
+}
+
+function setTextPassengersParents ( inputValue ) {
+  switch ( inputValue ) {
+    case 1:
+      return 'взрослый';
+      break;
+    default:
+      return 'взрослых';
+      break;
+  }
+}
+
+function setTextPassengersChilds ( inputValue ) {
+  switch ( inputValue ) {
+    case 1:
+      return 'ребёнок';
+      break;
+    default:
+      return 'детей';
+      break;
+  }
+}
