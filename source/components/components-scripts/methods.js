3fa599ede8fbdfb07bce0dd5a1e4611c9d573bd8
Alexandr Ja <mr-spinn@outlook.com>
2017-03-13 09:01:35 +0600

    add module pattern for methods.js

--- source/components/components-scripts/methods.js
+++ source/components/components-scripts/methods.js
@@ -1,3 +1,15 @@
 function getMaxOfArray(arr) {
-	return Math.max.apply(null, arr);
+  return Math.max.apply(null, arr);
 }
+
+
+//todo сделать модули для всех компонентов
+(function () {
+
+
+
+
+
+  
+})();
+
