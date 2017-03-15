00a5f65b34cf25231230e77458c7f405da2e768f
Alexandr Ja <mr-spinn@outlook.com>
2017-03-15 16:51:48 +0600

    add properties for addSelectElement function

--- source/components/components-scripts/methods.js
+++ source/components/components-scripts/methods.js
@@ -11,6 +11,7 @@ function getMaxOfArray(arr) {
 
 
 function addSelectElement ( event, ratingWrapperId, placeholderId, ratingInputId, removeButtonId ) {
+  console.log( 'start addSelectElement' );
   /*
    1. Пользвователь нажимает на выбранный рейтинг
    2. Скрипт проверяет виден ли placeholder, если виден то прячем - добавляем классс hide
@@ -31,8 +32,9 @@ function addSelectElement ( event, ratingWrapperId, placeholderId, ratingInputId
   // обёртки и для кнопки удаления
   // Пока элемент не вставлен на страницу то идентификатор по прежнему остаётся уникальным
 
-  //После клонирования удаляем id для обёртки и для кнопки удаления из оригинальных HTML тегов - которые существуют в разметке ( Часть 1/2 )
-  document.getElementById( removeButtonId ).removeAttribute('id');
+  //После клонирования обновляем id для обёртки и для кнопки удаления из оригинальных HTML тегов - которые существуют в разметке ( Часть 1/3 )
+  var removeButtonIdInDropdown = removeButtonId + '-in-dropdown';
+  document.getElementById( removeButtonId ).setAttribute( 'id', removeButtonIdInDropdown );
 
 
   if ( ratingInput.contains( ratingWrapper ) ) {
@@ -41,19 +43,30 @@ function addSelectElement ( event, ratingWrapperId, placeholderId, ratingInputId
     return false;
   }
 
-  //После клонирования удаляем id для обёртки и для кнопки удаления из оригинальных HTML тегов - которые существуют в разметке ( Часть 2/2 )
-  ratingWrapper.removeAttribute('id');
+  //После клонирования обновляем id для обёртки и для кнопки удаления из оригинальных HTML тегов - которые существуют в разметке ( Часть 2/3 )
+  var ratingWrapperIdInDropdown = ratingWrapperId + '-in-dropdown';
+  ratingWrapper.setAttribute( 'id', ratingWrapperIdInDropdown );
+
+  //Запоминаем событие которое было установлено для item'a в dropdown и удаляем это событие из клона ( Часть 3/3 )
+  // var ratingWrapperIdEventInDropdown = ratingWrapper.getAttribute('onclick');
+  ratingWrapperClone.removeAttribute('onclick');
+  // console.log( ratingWrapperIdEventInDropdown );
 
 
+  // addSelectElement.REMOVE_BUTTON_ID_IN_DROPDOWN        = removeButtonIdInDropdown;
+  // addSelectElement.RATING_WRAPPER_ID_IN_DROPDOWN       = ratingWrapperIdInDropdown;
+  // addSelectElement.RATING_WRAPPER_ID_EVENT_IN_DROPDOWN = ratingWrapperIdEventInDropdown;
+  
+  
   //После удаления повторяющихся id вставляем клона
   ratingInput.appendChild( ratingWrapperClone );
+
 
   //снимаем фиксированную высоту с контейнеру куда вставился клон и устанавливаем минимальную выслоту
   //todo: т.к. используем flex то можно задавать высоту только  родителю для input, но тогда будут увеличиваться по высоте все input в родителе у которого меняется высота
   var ratingInputBeforeAddtem = ratingInput.offsetHeight;
   ratingInput.style.height = 'auto';
   ratingInput.style.minHeight = ratingInputBeforeAddtem + 'px';
-  // console.log( ratingInput.offsetHeight );
 
 
   //показываем кнопку удаления
@@ -63,17 +76,30 @@ function addSelectElement ( event, ratingWrapperId, placeholderId, ratingInputId
   recalculationTabHeight();
 }
 
+//ratingWrapperIdInDropdown, removeButtonIdInDropdown
+
+function removeSelectElement( event, ratingWrapperId, placeholderId, dropdownId ) {
+  var removeButton              = document.getElementById( event.target.id );
+  var ratingWrapperIdInDropdown = ratingWrapperId + '-in-dropdown';
+  var placeholder               = document.getElementById( placeholderId );
+  var dropdown                  = document.getElementById( dropdownId );
 
 
-function removeSelectElement( event, ratingWrapperId, placeholderId, ratingInputId ) {
-  var ratingWrapper      = document.getElementById( ratingWrapperId );
-  var placeholder        = document.getElementById( placeholderId );
-  var ratingInput        = document.getElementById( ratingInputId );
+  console.log( ratingWrapperId );
+
+  var ratingWrapper
+
+  /*
+    1. Удаляем элемент из родителя
+    2. Заменяем id у обёртки и кнопки на оригинальные
+   */
+
+
 
 
   ratingWrapper.parentNode.removeChild( ratingWrapper );
 
-
+  // recalculationTabHeight();
 
 }
 
