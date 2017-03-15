cd2b2dd9dc86c674a689337ce5380de9cbbb0640
Alexandr Ja <mr-spinn@outlook.com>
2017-03-15 11:26:53 +0600

    cloone id elements error

--- source/components/components-scripts/methods.js
+++ source/components/components-scripts/methods.js
@@ -12,11 +12,23 @@ function getMaxOfArray(arr) {
 
 function addSelectElement ( event, ratingWrapperId, placeholderId, ratingInputId, removeButtonId ) {
   var ratingWrapper              = document.getElementById( ratingWrapperId );
+  if ( document.getElementById( ratingWrapperId ) ) {
+    ratingWrapper.removeAttribute('id');
+    var ratingWrapperClone         = ratingWrapper.cloneNode( true );
+  }
 
-  var ratingWrapperClone         = ratingWrapper.cloneNode( true );
-
+  // Делаем копию рейтинга, при этом id тоже копируется и документ содержит уже два элемента с одним идентификатором для
+  // обёртки и для кнопки
+  // Пока элемент не вставлен на страницу то идентификатор по прежнему остаётся уникальным
 
   var removeButton = document.getElementById( removeButtonId );
+  // var removeButtonInWrapperClone = ratingWrapperClone.getElementById( removeButtonId );
+
+
+  console.log( ratingWrapper );
+  console.log( removeButton );
+  // console.log( removeButtonInWrapperClone );
+
 
   var placeholder                = document.getElementById( placeholderId );
   var ratingInput                = document.getElementById( ratingInputId );
@@ -24,24 +36,20 @@ function addSelectElement ( event, ratingWrapperId, placeholderId, ratingInputId
 
   /*
   1. Пользвователь нажимает на выбранный рейтинг
-  2. Скрипт проверяет содержит ли контейнер выбранный рейтинг, если содержит, то выходим из функции
-                                                               если не содержит прячем placeholder
+  2. Скрипт проверяет виден ли placeholder, если виден то прячем - добавляем классс hide
    */
-
-  // console.log( ratingInput ); 
 
   if ( ratingInput.contains( ratingWrapper ) ) {
     //Чтобы не вставлялись копии уже вставленных ratingWrapper'ов
     return false;
   }
 
-
   if ( !placeholder.classList.contains('hide') ) {
     //Если placeholderId виден
    placeholder.classList.add('hide');
   }
 
-  ratingWrapper.removeAttribute('id');
+
   removeButton.classList.remove('hide');
 
   ratingInput.appendChild( ratingWrapperClone );
