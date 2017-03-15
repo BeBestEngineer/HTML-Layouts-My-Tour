f5da55c020f07cd5c506f134ad0dc8ef8acaf14f
Alexandr Ja <mr-spinn@outlook.com>
2017-03-15 20:39:11 +0600

    delete dynamic create list for hotel-rating dropdown

--- source/components/components-scripts/elements_hotels-rating.js
+++ source/components/components-scripts/elements_hotels-rating.js
@@ -1,24 +1,5 @@
 //todo сделать модули для всех компонентов
 (function () {
-  var ratingListOriginal 			= document.getElementById('hotels-rating-select__list-id');
-  var ratingItemOriginal 		  = document.getElementById('hotels-rating-select__item-id');
-	var ratingItemOriginalValue 	= document.getElementById('hotels-rating-select__value-id');
-  var ratingItemModified;
-  var ratingItemsFragment  		= document.createDocumentFragment();
-
-
-  ratingItemOriginal.classList.add('hotels-rating-list__item', 'li');
-	ratingItemOriginal.removeAttribute('id');
-	ratingItemOriginalValue.removeAttribute('id');
-
-  var i;
-  for ( i = 1; i <= 5; i++) {
-		ratingItemOriginalValue.innerHTML = i;
-		ratingItemModified = ratingItemOriginal.cloneNode(true);
-		ratingItemsFragment.appendChild( ratingItemModified );
-  }
-
-	ratingListOriginal.innerHTML = '';
-  ratingListOriginal.appendChild( ratingItemsFragment );
+  
 
 })();
