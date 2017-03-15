d176cf7dc3bb982ed0dc492e28dd1a7412a756f8
Alexandr Ja <mr-spinn@outlook.com>
2017-03-15 08:29:51 +0600

    add function for select elements "addSelectElement"

--- source/components/components-scripts/methods.js
+++ source/components/components-scripts/methods.js
@@ -7,9 +7,41 @@ function getMaxOfArray(arr) {
 (function () {
 
 
-
-
-
-  
 })();
+
+
+function addSelectElement ( event, ratingWrapperId, placeholderId, ratingInputId ) {
+  /*
+
+
+
+  var rating      = document.getElementById( ratingId );
+  var ratingClone = rating.cloneNode( true );
+  var ratingInput = document.getElementById( ratingInputId );
+  var ratingInputText = ratingInput.getElementsByTagName( 'span' );
+
+
+  if ( ratingInput.contains( rating ) ) {
+    return false;
+  }
+
+  if ( ratingInputText ) {
+    //Если поле содержит плейсхолдер удаляем его - лучше скрыть через display:none
+    for ( i = 0; i < ratingInputText.length; i++ ) {
+      var ratingInputTextItem = ratingInputText[i];
+      //не надо искать по коллекции,  устаyjdb id для palceholder и обращайся к нему
+      ratingInput.removeChild( ratingInputTextItem );
+    }
+  }
+
+  ratingInput.appendChild( ratingClone );
+  */
+
+}
+
+
+
+function removeSelectElement() {
+  
+}
 
