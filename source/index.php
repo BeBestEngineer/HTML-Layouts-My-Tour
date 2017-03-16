1770c677f6c745d4d4b684bbe6249a8fc4b4feff
Alexandr Ja <mr-spinn@outlook.com>
2017-03-16 18:42:55 +0600

    add session functions for test "send form data"

--- source/index.php
+++ source/index.php
@@ -4,6 +4,17 @@ error_reporting(E_ERROR|E_WARNING|E_PARSE|E_NOTICE|E_ALL);
 ini_set('display_errors', 1);
 
 
+
+
+session_start();
+
+if ( isset( $_POST['Button_pressed'] ) ) {
+    $_SESSION = $_POST;
+}
+
+
+
+
 if ( isset( $_GET['page'] ) ) {    
     
     require_once 'pages/page-head.php';
@@ -13,6 +24,11 @@ if ( isset( $_GET['page'] ) ) {
 } else {
     header('Location: '.$_SERVER['REQUEST_URI'].'?page=interests');
 }
+
+
+
+
+$_SESSION[] = '';
 
 
 ?>
