5bdf32a970cc3528f45fb6652397d4c45322d739
Alexandr Ja <mr-spinn@outlook.com>
2017-03-18 19:31:44 +0600

    change id for countries--africa modal window

--- source/modules/modules-widgets/countries-modal-window/scripts/countries-modal-window.js
+++ source/modules/modules-widgets/countries-modal-window/scripts/countries-modal-window.js
@@ -1,11 +1,12 @@
+
 (function () {
-	var modal = document.getElementById('modal-windows__countries-list');
+	var modal = document.getElementById('modal-windows__countries-list--africa');
 
 // Get the button that opens the modal
-	var btn = document.getElementById("regions__modal-window--button");
+	var btn = document.getElementById("countries--africa__modal-window-button");
 
 // Get the <span> element that closes the modal
-	var span = document.getElementsByClassName("close")[0];
+	var span = document.getElementById("countries--africa__modal-window-close-button");
 
 // When the user clicks the button, open the modal
 	btn.onclick = function() {
@@ -19,11 +20,12 @@
 
 // When the user clicks anywhere outside of the modal, close it
 	window.onclick = function(event) {
+		// console.log( 'countries - ' + event.target );
+		// console.log( 'countries - ' + modal );
+//todo: onclick для cities перезаписывается, поэтому не закрываются dropdowsn
+
 		if (event.target == modal) {
 			modal.style.display = "none";
 		}
 	}
-
-	//todo: добавить дерево стран -> дерево регионов и стран
-
 })();
