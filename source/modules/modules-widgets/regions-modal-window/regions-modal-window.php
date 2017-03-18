44761cdd6a48d285f2c1ee1bda035a702e02ec44
Alexandr Ja <mr-spinn@outlook.com>
2017-03-18 18:09:30 +0600

    add markup for modal window for regions

---
+++ source/modules/modules-widgets/regions-modal-window/regions-modal-window.php
@@ -0,0 +1,100 @@
+<!-- The Modal -->
+<div id="modal-windows__regions-list" class="modal">
+
+  <!-- Modal content -->
+  <div class="modal-content">
+    <div class="modal-header">
+      <span class="close">&times;</span>
+    </div>
+    <div class="">
+      <ul>
+<!--        todo: распарсить через дерево-->
+        <li>
+          <label class="custom-checkbox__label">
+            <span id="regions-australia-and-oceania--label-text-id">Австралия и Океания</span>
+
+            <input class="custom-checkbox__input"
+                   id="regions-australia-and-oceania--input-id"
+                   onclick="addCheckedElement( event, 'regions-australia-and-oceania--label-text-id', 'pills__wrapper--regions-id', 'pills__text--regions-id', 'pills__button--regions-id', 'regions__modal-window--button', 'regions__modal-window--placeholder-id' )"
+                   type="checkbox"
+                   name="regions-australia-and-oceania" >
+                    <span class="cr">
+                      <i class="cr-icon custom-checkbox__icon"></i>
+                    </span>
+          </label>
+        </li>
+        <li>
+          <label class="custom-checkbox__label">
+            <span id="regions-asia--label-text-id">Азия</span>
+
+            <input class="custom-checkbox__input"
+                   id="regions-asia--input-id"
+                   onclick="addCheckedElement( event, 'regions-asia--label-text-id', 'pills__wrapper--regions-id', 'pills__text--regions-id', 'pills__button--regions-id', 'regions__modal-window--button', 'regions__modal-window--placeholder-id' )"
+                   type="checkbox"
+                   name="regions-asia" >
+                    <span class="cr">
+                      <i class="cr-icon custom-checkbox__icon"></i>
+                    </span>
+          </label>
+        </li>
+        <li>
+          <label class="custom-checkbox__label">
+            <span id="regions-america--label-text-id">Америка</span>
+
+            <input class="custom-checkbox__input"
+                   id="regions-america--input-id"
+                   onclick="addCheckedElement( event, 'regions-america--label-text-id', 'pills__wrapper--regions-id', 'pills__text--regions-id', 'pills__button--regions-id', 'regions__modal-window--button', 'regions__modal-window--placeholder-id' )"
+                   type="checkbox"
+                   name="regions-america" >
+                    <span class="cr">
+                      <i class="cr-icon custom-checkbox__icon"></i>
+                    </span>
+          </label>
+        </li>
+        <li>
+          <label class="custom-checkbox__label">
+            <span id="regions-africa--label-text-id">Африка</span>
+
+            <input class="custom-checkbox__input"
+                   id="regions-africa--input-id"
+                   onclick="addCheckedElement( event, 'regions-africa--label-text-id', 'pills__wrapper--regions-id', 'pills__text--regions-id', 'pills__button--regions-id', 'regions__modal-window--button', 'regions__modal-window--placeholder-id' )"
+                   type="checkbox"
+                   name="regions-africa" >
+                    <span class="cr">
+                      <i class="cr-icon custom-checkbox__icon"></i>
+                    </span>
+          </label>
+        </li>
+        <li>
+          <label class="custom-checkbox__label">
+            <span id="regions-europe--label-text-id">Европа</span>
+
+            <input class="custom-checkbox__input"
+                   id="regions-europe--input-id"
+                   onclick="addCheckedElement( event, 'regions-europe--label-text-id', 'pills__wrapper--regions-id', 'pills__text--regions-id', 'pills__button--regions-id', 'regions__modal-window--button', 'regions__modal-window--placeholder-id' )"
+                   type="checkbox"
+                   name="regions-europe" >
+                    <span class="cr">
+                      <i class="cr-icon custom-checkbox__icon"></i>
+                    </span>
+          </label>
+        </li>
+        <li>
+          <label class="custom-checkbox__label">
+            <span id="regions-euroasia--label-text-id">Евразия</span>
+
+            <input class="custom-checkbox__input"
+                   id="regions-euroasia--input-id"
+                   onclick="addCheckedElement( event, 'regions-euroasia--label-text-id', 'pills__wrapper--regions-id', 'pills__text--regions-id', 'pills__button--regions-id', 'regions__modal-window--button', 'regions__modal-window--placeholder-id' )"
+                   type="checkbox"
+                   name="regions-euroasia" >
+                    <span class="cr">
+                      <i class="cr-icon custom-checkbox__icon"></i>
+                    </span>
+          </label>
+        </li>
+      </ul>
+    </div>
+  </div>
+
+</div>
