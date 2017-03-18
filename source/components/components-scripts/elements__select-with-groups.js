7e9db5d1a735b5ca2691019de55322302aae2ff0
Alexandr Ja <mr-spinn@outlook.com>
2017-03-18 18:02:09 +0600

    add file for select group region and countries scripts

---
+++ source/components/components-scripts/elements__select-with-groups.js
@@ -0,0 +1,37 @@
+/*
+Исходные данные:
+	1. Две кнопки: "Добавить регион" и "Добавить страну"
+	2. Поле для визуализации результатов выбора
+
+	1. При нажатии на кнопку  "Добавить регион" открывается модальное окно и выбитрается регион
+	2. При нажатии на кнопку  "Добавить страну" открывается модальное окно и выбитрается страна с учётом региона
+*/
+
+(function () {
+
+	// function Log() {
+	// 	console.log('module log');
+	// }
+	//
+	// Log();
+
+	var addRegionButton = document.getElementById( 'form__add-region-button-id' );
+
+
+
+
+})();
+
+
+
+
+function addRegion() {
+	console.log('addRegion log');
+}
+
+
+
+
+function addCountry() {
+	console.log('addCountry log');
+}
