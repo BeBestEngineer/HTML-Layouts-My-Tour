229a07ba33465392f40372be709ac21bdb5b1f6d
Alexandr Ja <mr-spinn@outlook.com>
2017-03-19 10:11:03 +0600

    update addRegion function: overwriting id button for countries

--- source/components/components-scripts/elements__select-with-groups.js
+++ source/components/components-scripts/elements__select-with-groups.js
@@ -25,8 +25,35 @@
 
 
 
-function addRegion() {
-	console.log('addRegion log');
+function addRegion( event, labelTextId, pillWrapperId, pillTextId, pillButtonId, dropdownButtonId, placeholderId, FirstLevelDependentButtonId ) {
+
+	var FirstLevelDependentButtonNewId = FirstLevelDependentButtonId.slice(0, 11) + labelTextId.slice(8, -15) + FirstLevelDependentButtonId.slice(-21);
+	// console.log( FirstLevelDependentButtonNewId );
+
+	if ( document.getElementById( FirstLevelDependentButtonId ) ) {
+		//Если существует кнопка с исходным id, то меняем на id соответствующего региона
+		document.getElementById(FirstLevelDependentButtonId).id = FirstLevelDependentButtonNewId;
+
+		// и записываем id установленного региона в свойство функции, чтобы при добавлении нового региона можно было найти кнопку для добавления стран и перезаписать ей id
+		addRegion.FIRST_LEVEL_DEPENDENT_BUTTON_NEW_ID = FirstLevelDependentButtonNewId;
+	}
+
+	// if ( !document.getElementById( FirstLevelDependentButtonId ) ) {
+		//Если не существует кнопка с исходным id, то
+		//Перезаписываем id добавленного региона на id региона который добавляем
+		document.getElementById( addRegion.FIRST_LEVEL_DEPENDENT_BUTTON_NEW_ID ).id = FirstLevelDependentButtonNewId;
+	// }
+
+
+	addCheckedElement( event, labelTextId, pillWrapperId, pillTextId, pillButtonId, dropdownButtonId, placeholderId );
+}
+
+
+function removeRegion ( event, placeholderId, pillWrapperClass ) {
+
+	removeCheckedElement( event, placeholderId, pillWrapperClass );
+
+
 }
 
 
