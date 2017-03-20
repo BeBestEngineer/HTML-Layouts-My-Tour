5d7376cca1a7a8246a3e55c6bd52bb9d48b44bac
Alexandr Ja <mr-spinn@outlook.com>
2017-03-20 10:38:27 +0600

    add description for addCheckedElement function, change id for pills_wrapper countries

--- source/modules/modules-widgets/countries-modal-window/countries-modal-window--africa.php
+++ source/modules/modules-widgets/countries-modal-window/countries-modal-window--africa.php
@@ -1,5 +1,5 @@
 <!-- The Modal -->
-<div id="modal-windows__regions--africa-list" class="modal">
+<div id="modal-windows__countries--africa-list" class="modal">
 
   <!-- Modal content -->
   <div class="modal-content">
@@ -9,13 +9,32 @@
     <div class="">
       <ul>
         <!--        todo: распарсить через дерево-->
+
+<!--               onclick="addCheckedElement(
+                                                event,
+                                                'countries-aljir--label-text-id',         - id текста который вставляется в пилюлю
+                                                'pills__wrapper--countries-id',    - id пилюли контейнера для вставки стран
+                                                'pills__text--countries-id',                -
+                                                'pills__button--countries-id',              -
+                                                'countries__modal-window--button',          -
+                                                'countries__modal-window--placeholder-id'   -
+                                              )"
+-->
         <li>
           <label class="custom-checkbox__label">
-            <span id="regions-aljir--label-text-id">Алжир</span>
+            <span id="countries-aljir--label-text-id">Алжир</span>
 
             <input class="custom-checkbox__input"
-                   id="regions-aljir--input-id"
-                   onclick="addCheckedElement( event, 'regions-aljir--label-text-id', 'pills__wrapper--regions-id', 'pills__text--regions-id', 'pills__button--regions-id', 'regions__modal-window--button', 'regions__modal-window--placeholder-id' )"
+                   id="countries-aljir--input-id"
+                   onclick="addCheckedElement(
+                                                event,
+                                                'countries-aljir--label-text-id',
+                                                'pills__wrapper--countries-africa-id',
+                                                'pills__text--countries-id',
+                                                'pills__button--countries-id',
+                                                'countries__modal-window--button',
+                                                'countries__modal-window--placeholder-id'
+                                              )"
                    type="checkbox"
                    name="regions-aljir" >
                     <span class="cr">
@@ -29,7 +48,7 @@
 
             <input class="custom-checkbox__input"
                    id="regions-egipet--input-id"
-                   onclick="addCheckedElement( event, 'regions-egipet--label-text-id', 'pills__wrapper--regions-id', 'pills__text--regions-id', 'pills__button--regions-id', 'regions__modal-window--button', 'regions__modal-window--placeholder-id' )"
+                   onclick="addCheckedElement( event, 'regions-egipet--label-text-id', 'pills__wrapper--countries-africa-id', 'pills__text--regions-id', 'pills__button--regions-id', 'regions__modal-window--button', 'regions__modal-window--placeholder-id' )"
                    type="checkbox"
                    name="regions-egipet" >
                     <span class="cr">
@@ -43,7 +62,7 @@
 
             <input class="custom-checkbox__input"
                    id="regions-madagaskar--input-id"
-                   onclick="addCheckedElement( event, 'regions-madagaskar--label-text-id', 'pills__wrapper--regions-id', 'pills__text--regions-id', 'pills__button--regions-id', 'regions__modal-window--button', 'regions__modal-window--placeholder-id' )"
+                   onclick="addCheckedElement( event, 'regions-madagaskar--label-text-id', 'pills__wrapper--countries-africa-id', 'pills__text--regions-id', 'pills__button--regions-id', 'regions__modal-window--button', 'regions__modal-window--placeholder-id' )"
                    type="checkbox"
                    name="regions-madagaskar" >
                     <span class="cr">
