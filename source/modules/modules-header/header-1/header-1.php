8bc6f4601eba3d3f85780252aeff25e243184009
Alexandr Ja <mr-spinn@outlook.com>
2017-03-23 20:31:28 +0600

    refactor heder-1: add flex grid

--- source/modules/modules-header/header-1/header-1.php
+++ source/modules/modules-header/header-1/header-1.php
@@ -1,36 +1,46 @@
-<header class="header-1  flex__container">
-  <div class="flex__container  grid__container--medium">
-    <nav class="flex__item--stretch  grid__col  main-nav">
-      <ul>
-        <li class="main-nav__item">
-          <a class="links  main-nav__link" href="#">Моя лента</a>
-        </li>
-        <li class="main-nav__item  main-nav__item--active">
-          <a class="links  main-nav__link" href="#">Интересы</a>
-        </li>
-        <li class="main-nav__item">
-          <a class="links  main-nav__link" href="#">Направления</a>
-        </li>
-        <li class="main-nav__item">
-          <a class="links  main-nav__link" href="#">Подбор тура</a>
-        </li>
-        <li class="main-nav__item">
-          <a class="links  main-nav__link" href="#">Заявка</a>
-        </li>
-      </ul>
-    </nav>
+<header class="header-1  ">
+  <div class="container">
+    <div class="row">
+      <div class="col  col-lg-16  col-md-16  col-sm-12  col-xs-20">
+<!--        col-lg-16  col-md-16  col-sm-20  col-xs-20-->
+
+        <nav class="flex__item--stretch  main-nav">
+          <ul>
+            <li class="main-nav__item">
+              <a class="links  main-nav__link" href="#">Моя лента</a>
+            </li>
+            <li class="main-nav__item  main-nav__item--active">
+              <a class="links  main-nav__link" href="#">Интересы</a>
+            </li>
+            <li class="main-nav__item">
+              <a class="links  main-nav__link" href="#">Направления</a>
+            </li>
+            <li class="main-nav__item">
+              <a class="links  main-nav__link" href="#">Подбор тура</a>
+            </li>
+            <li class="main-nav__item">
+              <a class="links  main-nav__link" href="#">Заявка</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+
+      <aside class="col--test  col  col-lg-2   col-md-2   col-sm-4   col-xs-20  col--order--1">
+        <!--        col-lg-2   col-md-5   col-sm-10  col-xs-20  col--order--1-->
+
+        <a href="#" class="logo-link  image-wrapper">
+          <img class="image" src="images/logo/logo.jpg" alt="logo">
+        </a>
+      </aside>
+
+
+      <aside class="col--test  col  col-lg-2   col-md-2   col-sm-4   col-xs-20">
+        <!--        col-lg-2   col-md-5   col-sm-10  col-xs-20-->
+        Empty Aside
+      </aside>
+    </div>
+    <div class="row">Empty row 1</div>
+    <div class="row">Empty row 2</div>
   </div>
-
-
-  <aside class="flex__item--equal-width  grid__col  flex__item--order--1  flex__container  flex__justify-content--flex-end">
-<!--    Empty Aside-->
-    <a href="#" class="logo-link  image-wrapper">
-      <img class="image" src="images/logo/logo.jpg" alt="logo">
-    </a>
-  </aside>
-
-
-  <aside class="flex__item--equal-width  grid__col">
-<!--    Empty Aside-->
-  </aside>
 </header>
