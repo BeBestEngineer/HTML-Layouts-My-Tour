47bcafd181a5e71e3818ab51b5b21d1e708cb817
Alexandr Ja <mr-spinn@outlook.com>
2017-03-24 18:33:28 +0600

    refactor footer-1 markup

--- source/modules/modules-footer/footer-1/footer-1.php
+++ source/modules/modules-footer/footer-1/footer-1.php
@@ -1,93 +1,108 @@
 <footer class="footer-1  flex__container">
-  <div class="grid__container--large">
-    <div class="flex__container  ">
-      <div class="flex__item--equal-width  grid__col">
-        <b class="company-name">Туристический проект</b>
-        <p>
-          Москва, 1-й Щипковский переулок, д.1
-        </p>
-        <a class="links  border__bottom--dotted" href="#">Показать на карте</a>
-      </div>
+
+  <div class="container">
+
+    <div class="row  footer-1__row-container">
+            <div class="col
+                        col-lg-4
+                        col-md-5
+                        col-sm-8
+                        col-xs-20">
+              <b class="col-title">Туристический проект</b>
+              <p>
+                Москва, 1-й Щипковский переулок, д.1
+              </p>
+              <a class="links  border__bottom--dotted" href="#">Показать на карте</a>
+            </div>
 
 
-      <div class="flex__item--equal-width  grid__col">
-        <p>
-          Время работы офиса: <br>
-          Будни 10:00 - 21:00 <br>
-          Выходные и праздники 11:00 - 17:00
-        </p>
-      </div>
+            <div class="col
+                        col-lg-4  col-lg-offset-3
+                        col-md-4  col-md-offset-2
+                        col-sm-6
+                        col-xs-20">
+              <b class="col-title"></b>
+              <p>
+                Время работы офиса: <br>
+                Будни 10:00 - 21:00 <br>
+                Выходные и праздники 11:00 - 17:00
+              </p>
+            </div>
 
 
-      <div class="flex__item--equal-width  grid__col">
-        <span> +7 495 585 55 55 </span> <br>
-        <a class="links  border__bottom--dotted" href="#">Обратная связь</a>
-      </div>
+            <div class="col
+                        col-lg-2  col-lg-offset-2
+                        col-md-3  col-md-offset-2
+                        col-sm-4  col-sm-offset-2
+                        col-xs-20">
+              <b class="col-title"></b>
+              <span> +7 495 585 55 55 </span> <br>
+              <a class="links  border__bottom--dotted" href="#">Обратная связь</a>
+            </div>
 
 
-      <div class="flex__item--equal-width  grid__col">
-        <ul class="social-links-list">
-          <li>
-            <a class="links" href="#">
+            <div class="col
+                        col-lg-3  col-lg-offset-2
+                        col-md-4  col-md-offset-0
+                        col-sm-20
+                        col-xs-20">
+              <b class="col-title"></b>
+              <div class="flex__container  flex__justify-content--flex-end">
+                <ul class="social-links-list">
+                <li>
+                  <a class="links" href="#">
               <span class="social-links-list__icon-wrapper  icons__wrapper  allign-middle">
                 <svg class="social-links-list__icon  icons" width="20" height="20" viewBox="0 0 1792 1792">
                   <path d="M1853 520q23 64-150 294-24 32-65 85-78 100-90 131-17 41 14 81 17 21 81 82h1l1 1 1 1 2 2q141 131 191 221 3 5 6.5 12.5t7 26.5-.5 34-25 27.5-59 12.5l-256 4q-24 5-56-5t-52-22l-20-12q-30-21-70-64t-68.5-77.5-61-58-56.5-15.5q-3 1-8 3.5t-17 14.5-21.5 29.5-17 52-6.5 77.5q0 15-3.5 27.5t-7.5 18.5l-4 5q-18 19-53 22h-115q-71 4-146-16.5t-131.5-53-103-66-70.5-57.5l-25-24q-10-10-27.5-30t-71.5-91-106-151-122.5-211-130.5-272q-6-16-6-27t3-16l4-6q15-19 57-19l274-2q12 2 23 6.5t16 8.5l5 3q16 11 24 32 20 50 46 103.5t41 81.5l16 29q29 60 56 104t48.5 68.5 41.5 38.5 34 14 27-5q2-1 5-5t12-22 13.5-47 9.5-81 0-125q-2-40-9-73t-14-46l-6-12q-25-34-85-43-13-2 5-24 17-19 38-30 53-26 239-24 82 1 135 13 20 5 33.5 13.5t20.5 24 10.5 32 3.5 45.5-1 55-2.5 70.5-1.5 82.5q0 11-1 42t-.5 48 3.5 40.5 11.5 39 22.5 24.5q8 2 17 4t26-11 38-34.5 52-67 68-107.5q60-104 107-225 4-10 10-17.5t11-10.5l4-3 5-2.5 13-3 20-.5 288-2q39-5 64 2.5t31 16.5z"/>
                 </svg>
               </span>
-              <span class="allign-middle">Вконтакте</span>
-            </a>
-          </li>
-          <li>
-            <a class="links" href="#">
+                    <span class="allign-middle">Вконтакте</span>
+                  </a>
+                </li>
+                <li>
+                  <a class="links" href="#">
               <span class="social-links-list__icon-wrapper  icons__wrapper  allign-middle">
                 <svg class="social-links-list__icon  icons" width="20" height="20" viewBox="0 0 1792 1792">
                   <path d="M1579 128q35 0 60 25t25 60v1366q0 35-25 60t-60 25h-391v-595h199l30-232h-229v-148q0-56 23.5-84t91.5-28l122-1v-207q-63-9-178-9-136 0-217.5 80t-81.5 226v171h-200v232h200v595h-735q-35 0-60-25t-25-60v-1366q0-35 25-60t60-25h1366z"/>
                 </svg>
               </span>
-              <span class="allign-middle">Facebook</span>
-            </a>
-          </li>
-          <li>
-            <a class="links" href="#">
+                    <span class="allign-middle">Facebook</span>
+                  </a>
+                </li>
+                <li>
+                  <a class="links" href="#">
               <span class="social-links-list__icon-wrapper  icons__wrapper  allign-middle">
                 <svg class="social-links-list__icon  icons" width="20" height="20" viewBox="0 0 1792 1792">
                   <path d="M1490 1426v-648h-135q20 63 20 131 0 126-64 232.5t-174 168.5-240 62q-197 0-337-135.5t-140-327.5q0-68 20-131h-141v648q0 26 17.5 43.5t43.5 17.5h1069q25 0 43-17.5t18-43.5zm-284-533q0-124-90.5-211.5t-218.5-87.5q-127 0-217.5 87.5t-90.5 211.5 90.5 211.5 217.5 87.5q128 0 218.5-87.5t90.5-211.5zm284-360v-165q0-28-20-48.5t-49-20.5h-174q-29 0-49 20.5t-20 48.5v165q0 29 20 49t49 20h174q29 0 49-20t20-49zm174-208v1142q0 81-58 139t-139 58h-1142q-81 0-139-58t-58-139v-1142q0-81 58-139t139-58h1142q81 0 139 58t58 139z"/>
                 </svg>
               </span>
-              <span class="allign-middle">Instagram</span>
-            </a>
-          </li>
-          <li>
-            <a class="links" href="#">
+                    <span class="allign-middle">Instagram</span>
+                  </a>
+                </li>
+                <li>
+                  <a class="links" href="#">
               <span class="social-links-list__icon-wrapper  icons__wrapper  allign-middle">
                 <svg class="social-links-list__icon  icons" enable-background="new 0 0 30 30" height="18px" viewBox="0 0 30 30" width="18px">
                   <path d="M22,15c-1,0-3,2-7,2s-6-2-7-2c-1.104,0-2,0.896-2,2c0,1,0.568,1.481,1,1.734C8.185,19.427,12,21,12,21l-4.25,5.438  c0,0-0.75,0.935-0.75,1.562c0,1.104,0.896,2,2,2c1.021,0,1.484-0.656,1.484-0.656S14.993,23.993,15,24  c0.007-0.007,4.516,5.344,4.516,5.344S19.979,30,21,30c1.104,0,2-0.896,2-2c0-0.627-0.75-1.562-0.75-1.562L18,21  c0,0,3.815-1.573,5-2.266C23.432,18.481,24,18,24,17C24,15.896,23.104,15,22,15z" id="K"/>
                   <path d="M15,0c-3.866,0-7,3.134-7,7s3.134,7,7,7c3.865,0,7-3.134,7-7S18.865,0,15,0z M15,10.5c-1.933,0-3.5-1.566-3.5-3.5  c0-1.933,1.567-3.5,3.5-3.5c1.932,0,3.5,1.567,3.5,3.5C18.5,8.934,16.932,10.5,15,10.5z" id="O"/>
                 </svg>
               </span>
-              <span class="allign-middle">Однокласники</span>
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
+                    <span class="allign-middle">Однокласники</span>
+                  </a>
+                </li>
+              </ul>
+              </div>
+            </div>
+          </div>
 
-
-    <div class="flex__container  flex__justify-content--flex-end">
-      <div class="grid__col">
-        <p class="">
+    <div class="row  flex__justify-content--flex-end">
+      <div class="col  col-lg-5  col-md-10  col-sm-20  col-xs-20">
+        <p class="text-right">
           Разработано командой TourAdmin &copy; 2017
         </p>
       </div>
     </div>
+
   </div>
 
-  <aside class="flex__item--equal-width  grid__col  flex__item--order--1">
-<!--    Empty Aside-->
-  </aside>
-
-
-  <aside class="flex__item--equal-width  grid__col">
-<!--    Empty Aside-->
-  </aside>
 </footer>
