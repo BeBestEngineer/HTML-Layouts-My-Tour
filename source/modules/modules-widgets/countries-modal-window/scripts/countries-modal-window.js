beeb9a8a792b88f3c0b9df87aa1281952e1e09fa
Alexandr Ja <mr-spinn@outlook.com>
2017-03-25 17:56:41 +0600

    update pattern for getElementById for countries lists

--- source/modules/modules-widgets/countries-modal-window/scripts/countries-modal-window.js
+++ source/modules/modules-widgets/countries-modal-window/scripts/countries-modal-window.js
@@ -1,9 +1,9 @@
-function openModal( event ) {
+function openModal( event ) { 
 	console.log( event.target.id );
 
 	var modal = document.getElementById( 'modal-windows__countries--' + event.target.id.slice( 11, -21 ) + '-list' );
 
-	var span = document.getElementById( 'countries--' + event.target.id.slice( 11, -21 ) + '__modal-window-close-button' );
+	var span  = document.getElementById( 'modal-windows__countries--' + event.target.id.slice( 11, -21 ) + '-close-button' );
 
 
 	modal.style.display = "block";
