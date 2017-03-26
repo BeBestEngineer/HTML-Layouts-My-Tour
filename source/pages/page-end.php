ba81c09200586a5dc64e78b4680c586e60cd2c21
Alexandr Ja <mr-spinn@outlook.com>
2017-03-26 19:21:10 +0600

    add script tags for include jQuery and Semantic UI scripts

--- source/pages/page-end.php
+++ source/pages/page-end.php
@@ -1,17 +1,24 @@
     <script src="//localhost:35729/livereload.js"></script>
     <!--third-components scripts-->
+    <!--68 кб jQuery -->
+    <script src="components/components-libs/jQuery/jquery.min.js"></script>
+    <script src="components/components-frameworks/SemanticUI/semantic.js"></script>
+<!--    <script src="components/components-frameworks/SemanticUI/dropdown.js"></script>-->
+
+
     <!--429 кб!!!!!!! - заменить-->
-<!--    <script src="components/components-libs/moment/moment.js"></script>-->
+    <!--<script src="components/components-libs/moment/moment.js"></script>-->
     <!--51 кб - заменить-->
     <script src="components/components-libs/moment/moment.min.js"></script>
-
-
-    <!--50 кб jQuery -->
-    <script src="components/components-libs/jQuery/jquery.min.js"></script>
-
     <script src="components/components-libs/pikaday/pikaday.js"></script>
+
+
     <script src="components/components-libs/noUIslider/nouislider.js"></script>
     <script src="components/components-libs/wnumb-1.1.0/wNumb.js"></script>
+
+
+
+
     <!--layout's scripts-->
     <script src="components/components-scripts/collected-scripts/layout.js"></script>
   </body>
