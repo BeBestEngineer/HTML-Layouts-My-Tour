2764a33bc043f745c187335b1ea1b7fc4c44db67
Alexandr Ja <mr-spinn@outlook.com>
2017-03-26 12:39:25 +0600

    add script file - moment.min.js instead moment.js

--- source/pages/page-end.php
+++ source/pages/page-end.php
@@ -1,6 +1,14 @@
     <script src="//localhost:35729/livereload.js"></script>
     <!--third-components scripts-->
-    <script src="components/components-libs/moment/moment.js"></script>
+    <!--429 кб!!!!!!! - заменить-->
+<!--    <script src="components/components-libs/moment/moment.js"></script>-->
+    <!--51 кб - заменить-->
+    <script src="components/components-libs/moment/moment.min.js"></script>
+
+
+    <!--50 кб jQuery -->
+    <script src="components/components-libs/jQuery/jquery.min.js"></script>
+
     <script src="components/components-libs/pikaday/pikaday.js"></script>
     <script src="components/components-libs/noUIslider/nouislider.js"></script>
     <script src="components/components-libs/wnumb-1.1.0/wNumb.js"></script>
