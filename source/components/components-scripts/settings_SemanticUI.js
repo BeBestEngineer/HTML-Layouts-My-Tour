5c752b3b9532cd58611874001e27697bfedc4ec5
Alexandr Ja <mr-spinn@outlook.com>
2017-03-27 08:52:44 +0600

    add settings for Semantic UI dropdowns

---
+++ source/components/components-scripts/settings_SemanticUI.js
@@ -0,0 +1,22 @@
+$('#interests-form__selection-dropdown--hotels-stars')
+		.dropdown({
+			allowAdditions: true
+		});
+
+
+$('#interests-form__selection-dropdown--hotels-rating')
+		.dropdown({
+			allowAdditions: true
+		});
+
+
+$('#interests-form__selection-dropdown--nutrition')
+		.dropdown({
+			allowAdditions: true
+		});
+
+
+$('#interests-form__selection-dropdown--locations')
+		.dropdown({
+			allowAdditions: true
+		});
