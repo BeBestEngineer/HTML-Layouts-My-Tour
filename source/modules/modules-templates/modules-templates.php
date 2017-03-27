8554c69b01d020e2ce2aa6de89b0352d7386fae8
Alexandr Ja <mr-spinn@outlook.com>
2017-03-27 08:56:56 +0600

    require icons--chevron-down-template

--- source/modules/modules-templates/modules-templates.php
+++ source/modules/modules-templates/modules-templates.php
@@ -1,1 +1,3 @@
-<?php require_once 'icons-templates/icons--close-template.php'?>
+<?php require_once 'icons-templates/icons--close-template/icons--close-template.php'?>
+
+<?php require_once 'icons-templates/icons--chevron-down-template/icons--chevron-down-template.php'?>
