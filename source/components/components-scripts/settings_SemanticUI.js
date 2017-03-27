ebf2bb337202b8d760b5fc483b0e079bb9d6a932
Alexandr Ja <mr-spinn@outlook.com>
2017-03-27 18:18:49 +0600

    add scripts for dropdown menus ( Semantic UI )

--- source/components/components-scripts/settings_SemanticUI.js
+++ source/components/components-scripts/settings_SemanticUI.js
@@ -1,22 +1,50 @@
+$('#passengers-dropdown-id')
+		.dropdown({
+			on: 'hover',
+			action: 'nothing'
+		})
+;
+
+
+$('#period-dropdown-id')
+		.dropdown({
+			on: 'hover',
+			action: 'nothing'
+		})
+;
+
+
+$('#price-dropdown-id')
+		.dropdown({
+			on: 'hover',
+			action: 'nothing'
+		})
+;
+
+
 $('#interests-form__selection-dropdown--hotels-stars')
 		.dropdown({
-			allowAdditions: true
+			allowAdditions: true,
+			on: 'hover'
 		});
 
 
 $('#interests-form__selection-dropdown--hotels-rating')
 		.dropdown({
-			allowAdditions: true
+			allowAdditions: true,
+			on: 'hover'
 		});
 
 
 $('#interests-form__selection-dropdown--nutrition')
 		.dropdown({
-			allowAdditions: true
+			allowAdditions: true,
+			on: 'hover'
 		});
 
 
 $('#interests-form__selection-dropdown--locations')
 		.dropdown({
-			allowAdditions: true
+			allowAdditions: true,
+			on: 'hover'			
 		});
